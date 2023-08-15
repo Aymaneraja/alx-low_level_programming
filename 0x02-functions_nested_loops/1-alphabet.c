eff0d0ff1104c3235339c547451bcd7f56d5482b
aymaneraja <aymanehs11@gmail.com>
2023-08-15 07:46:02 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* main - the Entry point the code, 
+* print_alphabet - the Entry point the code,
 *
 * Return : Always 0,
 */
@@ -9,6 +9,7 @@ void print_alphabet(void)
 for (char ch = 'a'; ch <= 'z'; ch++)
 {
 _putchar (ch);
+_putchar ('\n');
 }
 return (0);
 }
