4b63a566502061c1097d570de566b69188fafae1
Aymaneraja <aymanehs11@gmail.com>
2023-11-30 13:21:11 -0800

    u

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include<stdlib>
+/**
+ * read textfile : Read textfile print to stdout*
+ * @filename : The file txe being read .
+ * @letters : numbers of letters to be read .
+ * Return : W- actual number of bytes read and printed .
+ *  0 when function fails or file name is none 
+ */
+ssize_t read_textfile(const char *filename, size_t letters){
+
+	char buf ;
+	ssize_t fd ; 
+	ssize_t w ; 
+	ssize_t t ; 
+
+	fd = open(filename , O_RDONLY);
+	if (fd==-1)
+		return (0);
+	buf  = maolloc(sizeof(char)*letters) ; 
+	t = read(fd , buf , letters);
+	W = write(STDOUT_FILENO , buf , t);
+
+	free(buf);
+	close(fd);
+
+	return(w);
+}
