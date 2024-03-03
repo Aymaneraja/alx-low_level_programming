5040e8a5b18fcb07462e2c53bbeffbdeb4d95e1a
aym <aymanehs11@gmail.com>
2024-03-03 02:39:00 -0800

    e

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if(idx < 0 or idx > len(my_list)_1):
+        return None
+    else : 
+        new_list = my_list
+        new_list[idx] = element
+        return new_list
+
