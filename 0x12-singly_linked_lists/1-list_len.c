c0a7680d93e124e00160dc96fb42e3cc008b439b
aym <aymanehs11@gmail.com>
2024-03-06 06:35:59 -0800

    e

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,13 +1,12 @@
 size_t list_len(const list_t *h){
-	size_t count ; 
-	if(h->next == NULL) 
-		return 0;
-	else{
-		while(h->next)
-		{
-			count ++ ; 
-			h = h->next ; 
-		}
-		return count ; 
-	}
+size_t count ; 
+if(h->next == NULL) return 0;
+else{
+while(h->next)
+{
+count ++ ; 
+h = h->next ; 
+}
+return count ; 
+}
 }	
