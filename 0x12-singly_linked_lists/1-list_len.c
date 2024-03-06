51e09fa79d7f309ef5791ba8a2ed7121e8727e06
aym <aymanehs11@gmail.com>
2024-03-06 06:46:20 -0800

    e

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,13 +1,20 @@
-size_t list_len(const list_t *h){
-size_t count ; 
-if(h->next == NULL) 
-	return 0;
-else{
-	while(h->next)
-	{
-		count ++ ; 
-		h = h->next ; 
-	}
-	return count ; 
-}
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer to the list_t list
+ *
+ * Return: number of elements in h
+ */
+size_t list_len(const list_t *h)
+{
+ size_t n = 0;
+
+ while (h)
+ {
+ n++;
+ h = h->next;
+ }
+ return (n);
 }
